d07c7966069676ec8c356f4acacb834dc211a72b
RicardoPereiraDev <odracirDavidPereira@gmail.com>
2024-04-22 18:47:51 +0100

    add README

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -1,18 +1,28 @@
 package org.example;
 
-import java.sql.SQLOutput;
-
 public class Movie {
 
     String name;
     int yearOfRelease;
     boolean includeInPlan;
-    double avaliation;
+    double sumOfAvaliation;
     int totalOfAvaliations;
     int durationInMinutes;
 
     void displaysTechnicalFile(){
         System.out.println("Name of movie: " + name);
         System.out.println("The Year of release: " + yearOfRelease);
+
+    }
+    void evaluate(double note){
+        sumOfAvaliation += note;
+        totalOfAvaliations++;
+
+    }
+
+    double returnMediaOfAvaliations(){
+
+        double media = sumOfAvaliation/totalOfAvaliations;
+        return media;
     }
 }
--- src/main/java/org/example/Principal.java
+++ src/main/java/org/example/Principal.java
@@ -4,9 +4,6 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Principal {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        //System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
@@ -15,6 +12,19 @@ public class Principal {
         }
 
 
+
         Movie myMovie = new Movie();
+        myMovie.name = "The Godfather";
+        myMovie.yearOfRelease = 1984;
+        myMovie.durationInMinutes = 180;
+
+        myMovie.displaysTechnicalFile();
+        myMovie.evaluate(8);
+        myMovie.evaluate(10);
+        myMovie.evaluate(5);
+        System.out.println(myMovie.sumOfAvaliation);
+        System.out.println(myMovie.totalOfAvaliations);
+
+        System.out.println(String.format("Medium of total of avaliations is %.2f :" , myMovie.returnMediaOfAvaliations()));
     }
 }
